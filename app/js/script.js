c122ac299b8b5d8b186b3fdf9283726b06e6ba1a
AilsaYang <ailsayang831031@gmail.com>
2023-03-20 19:05:50 +0800

    feat: Add addToFavorite() function

--- app/js/script.js
+++ app/js/script.js
@@ -23,6 +23,8 @@ dataPanel.addEventListener("click", function onPanelClicked(event) {
   if (event.target.matches(".btn-show-movie")) {
     console.log(event.target.dataset.id);
     showMovieModal(event.target.dataset.id);
+  } else if (event.target.matches(".btn-add-favorite")) {
+    addToFavorite(Number(event.target.dataset.id));
   }
 });
 
@@ -65,8 +67,10 @@ function renderMovieList(data) {
           <h5 class="card-title">${item.title}</h5>
         </div>
         <div class="card-footer">
-          <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${item.id}">More</button>
-          <button class="btn btn-info btn-add-favorite">+</button>
+          <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${
+            item.id
+          }">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
         </div>
       </div>
     </div>
@@ -94,3 +98,26 @@ function showMovieModal(id) {
 }
 
 
+//addToFavorite function: to add favorite movies to localStorage
+function addToFavorite(id){
+  // 把localStorage裡原本用的資料取出，將 JSON 格式的字串轉回 JavaScript 原生物件，並把資料放到list裡
+  //如果localStorage裡面沒有資料則list= []
+  const favoriteMovieList =
+    JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+
+  //找出movies陣列中，item的id === id的資料，找到後會return這個item放到favMovie
+  const favMovie = movies.find((movie) => movie.id === id); //判斷數值
+  // console.log("You want to add", favMovie, "to favoriteMovieList"); //印出找到的那筆item
+
+  //看現在要加入的這個favMovie，在原本localStorage的list裡面是否已經有了，.some陣列方法，只要有部分符合，則為 true
+  if (favoriteMovieList.some((movie) => movie.id === id)) {
+    return alert("此電影已經在收藏清單中！");
+  } else alert(`電影:【${favMovie.title}】  \n收藏成功!!!`);
+
+  //更新favoriteMovieList這個Array
+  favoriteMovieList.push(favMovie);
+  // console.log("the favoriteMovieList now is:", favoriteMovieList);
+
+  //更新localStorage裡favoriteMovies這個key所對應到的新value(也就是更新後的favoriteMovieList)
+  localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovieList));
+}
